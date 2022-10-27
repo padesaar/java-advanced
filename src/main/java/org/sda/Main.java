047f977f7709f1584e07819e3e2914932afa77d8
Kätlin <kpadesaar@gmail.com>
2022-10-27 20:57:28 +0300

    Added exceptions

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -6,6 +6,9 @@ import org.sda.services.implementations.PersonServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
         //Interface
         Person person = new Person();
         person.setFirstName("Kätlin");
@@ -19,6 +22,28 @@ public class Main {
         System.out.println("Person's birth year:" + personService.getPersonBirthYear(person.getAge()));
         System.out.println("Person's full name:" + personService.getPersonFullName(person));
 
+        //EXEPTIONS
+        // Throwable - ctrl ja klikid, siis näitab throwable exeptionite klassi
+        //Exception sama lugu
+        //ArrayIndexOutOfBoundsException
+
+        //exception handling try and catch
+        try{
+            int[] integerArray = {1, 4, 56, 7};
+
+            for(int i = 0; i <= integerArray.length; i++) {
+                System.out.println(integerArray[i]);
+            }
+        } catch(ArrayIndexOutOfBoundsException e){ //e on variable name, just that
+            System.out.println("The number can't be printed because it's not available in array");
+        } catch (Exception exception) {
+            System.out.println("Exception being caught");
+        } finally { //this block will be executed irrespective of catch blocks
+            int a = 15;
+            System.out.println("Finally executed a:" + a);
+        }
+        // if you don't know what exception you may get,
+        //you put catch (Exception exception) sulgudesse
 
     }
 }
